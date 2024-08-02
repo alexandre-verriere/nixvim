8802c040ea36bc74b14302a8dd011adf38ee4df1
Alexandre Verriere <alexandre.verriere@gmail.com>
2024-08-02 15:20:42 +0200

    minor edits

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Home Manager configuration using flakes";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, }: {
+    homeConfigurations = {
+      alexandre = home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs { system = "x86_64-linux"; };
+
+        modules = [ ./home.nix ];
+      };
+    };
+  };
+}
